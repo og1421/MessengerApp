f1569b666489a0a5cc198afebff6170e47808e82
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-10-09 21:21:18 -0300

    Initial Commit

---
+++ MessengerApp/MessengerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerAppApp.swift
+//  MessengerApp
+//
+//  Created by Orlando Moraes Martins on 09/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MessengerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
